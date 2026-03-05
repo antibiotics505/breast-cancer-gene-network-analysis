bdb6cef7219b48722bd122bd169935da4d891e6c
antibiotics505 <sbren114@gmail.com>
2026-03-05 15:35:11 -0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,10 @@
+"""
+Please Read:
+
+We consulted Professor Sadia and were informed that it would be acceptable for our submission to
+take the form of a Jupyter notebook (.pynb) file. We have included all relevant project files in the
+OneDrive link found in our project proposal. Please refer to the project proposal for the OneDrive
+link as well as running instructions.
+
+Thank you so much for your understanding and support. We hope you enjoy our project!
+"""
